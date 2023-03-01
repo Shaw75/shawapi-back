76324658b97cd9d92872d8703d1653f70cb2a908
shaw775 <admin@shaw33.cc>
2023-03-01 22:25:30 +0800

    add shawapiclient-sdk and add online&offline to Controller

---
+++ src/main/java/cc/shaw33/project/common/IdRequest.java
@@ -0,0 +1,20 @@
+package cc.shaw33.project.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Id请求
+ *
+ * @author shaw
+ */
+@Data
+public class IdRequest implements Serializable {
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
